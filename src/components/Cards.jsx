5a49ee952ba4a56b3dce6a06ee8a2b5e4beb1612
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-09-06 20:12:16 +0500

    added cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cards() {
+  return (
+    <div className="w-full py-[10rem] px-4 bg-white">
+      <h1>Cards</h1>
+    </div>
+  );
+}
+
+export default Cards;
